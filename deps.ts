0aaef070bfbd3049736e915f632dc9a3f4e61ae1
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2021-05-29 17:21:33 +0200

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,9 @@
+export { 
+   applyGraphQL, 
+   gql, 
+   GQLError
+} from "https://deno.land/x/oak_graphql/mod.ts";
+
+export { Router, RouterContext } from "https://deno.land/x/oak@v6.2.0/mod.ts";
+
+export { Application } from "https://deno.land/x/oak@v6.2.0/mod.ts";
