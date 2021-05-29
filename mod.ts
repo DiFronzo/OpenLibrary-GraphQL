c03ce33eae55e1e43312ca367d385d4208de810d
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2021-05-29 17:23:35 +0200

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,10 @@
+import { Application } from './deps.ts'
+import {GraphQLService} from './src/schema.ts';
+
+const app = new Application();
+
+const gqlService = await GraphQLService('/graphql');
+app.use(gqlService.routes(), gqlService.allowedMethods());
+
+console.log("Server start at http://localhost:8080");
+await app.listen({ port: 8080 });
