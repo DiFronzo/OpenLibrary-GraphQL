8ee762282610eec583f0593d849eb9ee3d5da43e
DiFronzo <andrklie@stud.ntnu.no>
2021-05-30 14:26:07 +0200

    herouk

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-net=":$PORT" --allow-env mod.ts
--- mod.ts
+++ mod.ts
@@ -1,10 +1,11 @@
 import { Application } from "./deps.ts";
 import GraphQLService from "./src/schema.ts";
 
+const port = parseInt(Deno.env.get('PORT') ?? '8000');
 const app = new Application();
 
 const gqlService = await GraphQLService("/graphql");
 app.use(gqlService.routes(), gqlService.allowedMethods());
 
-console.log("Server start at http://localhost:8080");
-await app.listen({ port: 8080 });
+console.log("Server start at http://localhost:" + port);
+await app.listen({ port: port });
